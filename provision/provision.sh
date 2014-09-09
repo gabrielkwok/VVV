bb97b480ab5f577c4acbaad3f5f4c99fe6326649
Gabriel Kwok <gabriel@kiip.me>
2014-09-09 16:50:55 -0700

    Commented out the wordpress develop and trunk.

--- provision/provision.sh
+++ provision/provision.sh
@@ -470,70 +470,70 @@ if [[ $ping_result == *bytes?from* ]]; then
 		wp core upgrade
 	fi
 
-	# Test to see if an svn upgrade is needed
-	svn_test=$( svn status -u /srv/www/wordpress-develop/ 2>&1 );
-	if [[ $svn_test == *"svn upgrade"* ]]; then
-		# If the wordpress-develop svn repo needed an upgrade, they probably all need it
-		for repo in $(find /srv/www -maxdepth 5 -type d -name '.svn'); do
-			svn upgrade "${repo/%\.svn/}"
-		done
-	fi;
+# 	# Test to see if an svn upgrade is needed
+# 	svn_test=$( svn status -u /srv/www/wordpress-develop/ 2>&1 );
+# 	if [[ $svn_test == *"svn upgrade"* ]]; then
+# 		# If the wordpress-develop svn repo needed an upgrade, they probably all need it
+# 		for repo in $(find /srv/www -maxdepth 5 -type d -name '.svn'); do
+# 			svn upgrade "${repo/%\.svn/}"
+# 		done
+# 	fi;
 
-	# Checkout, install and configure WordPress trunk via core.svn
-	if [[ ! -d /srv/www/wordpress-trunk ]]; then
-		echo "Checking out WordPress trunk from core.svn, see http://core.svn.wordpress.org/trunk"
-		svn checkout http://core.svn.wordpress.org/trunk/ /srv/www/wordpress-trunk
-		cd /srv/www/wordpress-trunk
-		echo "Configuring WordPress trunk..."
-		wp core config --dbname=wordpress_trunk --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-define( 'WP_DEBUG', true );
-PHP
-		wp core install --url=local.wordpress-trunk.dev --quiet --title="Local WordPress Trunk Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
-	else
-		echo "Updating WordPress trunk..."
-		cd /srv/www/wordpress-trunk
-		svn up --ignore-externals
-	fi
+# 	# Checkout, install and configure WordPress trunk via core.svn
+# 	if [[ ! -d /srv/www/wordpress-trunk ]]; then
+# 		echo "Checking out WordPress trunk from core.svn, see http://core.svn.wordpress.org/trunk"
+# 		svn checkout http://core.svn.wordpress.org/trunk/ /srv/www/wordpress-trunk
+# 		cd /srv/www/wordpress-trunk
+# 		echo "Configuring WordPress trunk..."
+# 		wp core config --dbname=wordpress_trunk --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+# define( 'WP_DEBUG', true );
+# PHP
+# 		wp core install --url=local.wordpress-trunk.dev --quiet --title="Local WordPress Trunk Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
+# 	else
+# 		echo "Updating WordPress trunk..."
+# 		cd /srv/www/wordpress-trunk
+# 		svn up --ignore-externals
+# 	fi
 
-	# Checkout, install and configure WordPress trunk via develop.svn
-	if [[ ! -d /srv/www/wordpress-develop ]]; then
-		echo "Checking out WordPress trunk from develop.svn, see http://develop.svn.wordpress.org/trunk"
-		svn checkout http://develop.svn.wordpress.org/trunk/ /srv/www/wordpress-develop
-		cd /srv/www/wordpress-develop/src/
-		echo "Configuring WordPress develop..."
-		wp core config --dbname=wordpress_develop --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
-// Allow (src|build).wordpress-develop.dev to share the same database
-if ( 'build' == basename( dirname( __FILE__) ) ) {
-	define( 'WP_HOME', 'http://build.wordpress-develop.dev' );
-	define( 'WP_SITEURL', 'http://build.wordpress-develop.dev' );
-}
+# 	# Checkout, install and configure WordPress trunk via develop.svn
+# 	if [[ ! -d /srv/www/wordpress-develop ]]; then
+# 		echo "Checking out WordPress trunk from develop.svn, see http://develop.svn.wordpress.org/trunk"
+# 		svn checkout http://develop.svn.wordpress.org/trunk/ /srv/www/wordpress-develop
+# 		cd /srv/www/wordpress-develop/src/
+# 		echo "Configuring WordPress develop..."
+# 		wp core config --dbname=wordpress_develop --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+# // Allow (src|build).wordpress-develop.dev to share the same database
+# if ( 'build' == basename( dirname( __FILE__) ) ) {
+# 	define( 'WP_HOME', 'http://build.wordpress-develop.dev' );
+# 	define( 'WP_SITEURL', 'http://build.wordpress-develop.dev' );
+# }
 
-define( 'WP_DEBUG', true );
-PHP
-		wp core install --url=src.wordpress-develop.dev --quiet --title="WordPress Develop" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
-		cp /srv/config/wordpress-config/wp-tests-config.php /srv/www/wordpress-develop/
-		cd /srv/www/wordpress-develop/
-		npm install &>/dev/null
-	else
-		echo "Updating WordPress develop..."
-		cd /srv/www/wordpress-develop/
-		if [[ -e .svn ]]; then
-			svn up
-		else
-			if [[ $(git rev-parse --abbrev-ref HEAD) == 'master' ]]; then
-				git pull --no-edit git://develop.git.wordpress.org/ master
-			else
-				echo "Skip auto git pull on develop.git.wordpress.org since not on master branch"
-			fi
-		fi
-		npm install &>/dev/null
-	fi
+# define( 'WP_DEBUG', true );
+# PHP
+# 		wp core install --url=src.wordpress-develop.dev --quiet --title="WordPress Develop" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
+# 		cp /srv/config/wordpress-config/wp-tests-config.php /srv/www/wordpress-develop/
+# 		cd /srv/www/wordpress-develop/
+# 		npm install &>/dev/null
+# 	else
+# 		echo "Updating WordPress develop..."
+# 		cd /srv/www/wordpress-develop/
+# 		if [[ -e .svn ]]; then
+# 			svn up
+# 		else
+# 			if [[ $(git rev-parse --abbrev-ref HEAD) == 'master' ]]; then
+# 				git pull --no-edit git://develop.git.wordpress.org/ master
+# 			else
+# 				echo "Skip auto git pull on develop.git.wordpress.org since not on master branch"
+# 			fi
+# 		fi
+# 		npm install &>/dev/null
+# 	fi
 
-	if [[ ! -d /srv/www/wordpress-develop/build ]]; then
-		echo "Initializing grunt in WordPress develop... This may take a few moments."
-		cd /srv/www/wordpress-develop/
-		grunt
-	fi
+# 	if [[ ! -d /srv/www/wordpress-develop/build ]]; then
+# 		echo "Initializing grunt in WordPress develop... This may take a few moments."
+# 		cd /srv/www/wordpress-develop/
+# 		grunt
+# 	fi
 
 	# Download phpMyAdmin
 	if [[ ! -d /srv/www/default/database-admin ]]; then
